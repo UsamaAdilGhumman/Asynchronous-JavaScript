f5f5dd7bb4de06cec46b7b2c0a59699be0057008
UsamaAdilGhumman <usama.adil.ghumman@gmail.com>
2023-02-26 01:24:50 +0500

    ES6 Modules

---
+++ 17.js
@@ -0,0 +1,10 @@
+// ES6 Modules
+
+import {firstName} from './18.js'
+import {age} from './19.js'
+import Person  from './20.js';
+
+
+console.log(firstName,age);
+const person = new Person("Osama","Tariq",22);
+person.info();
